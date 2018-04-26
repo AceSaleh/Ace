6d57422867968a21d9b224658c2e05d96a7d99d7
CORP\Al.Woods <al.woods@jato.com>
2018-04-26 15:36:57 -0400

    Added Hello World Programs

---
+++ HelloWorld_2/HelloWorld_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("Please enter your name");
+            //Retrieve user input
+            
+            Console.WriteLine(/*Use string interpolation to output the message "Hello" and the persons name.  I.e, "Hello Al"*/);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld_Finished/HelloWorld_Finished/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld_Finished
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
